c8c715e676c4a951c2fcf50441d8b6cbd456a7e6
kyooo8 <kyosuke.7876@gmail.com>
2024-01-12 14:56:13 +0900

    ok

--- src/index.php
+++ src/index.php
@@ -8,27 +8,34 @@
 <body>
     ToDoリスト
     <?php
-    if(isset($_POST['title'],$_POST['row'],$_POST['due_date'],$_POST['category'])){
+    if(isset($_POST['title'], $_POST['row'], $_POST['due_date'], $_POST['category'])){
         require 'db.php';
         $pdo = new PDO($connect, USER, PASS);
 
-        $category_sql = $pdo->prepare('insert into Category (category_name) values (?)');
+        // カテゴリ追加
+        $category_sql = $pdo->prepare('INSERT INTO Category (category_name) VALUES (?)');
         $category_sql->execute([$_POST['category']]);
 
-        $category_id = $pdo->prepare('select category_id from Category where Category_name = ?');
-        $category_id->execute([$_POST['category']]);
-        $category_id = $category_id->fetchColumn();
+        // カテゴリID取得
+        $category_id_sql = $pdo->prepare('SELECT category_id FROM Category WHERE category_name = ?');
+        $category_id_sql->execute([$_POST['category']]);
+        $category_id = $category_id_sql->fetchColumn();
 
-        $task_sql = $pdo->prepare('insert into Task(title`row`,state,due_date,create_date,category_id)
-                                values(?,?,false,?,current_date(),?)');
-        $task_sql->execute([$_POST['title'],$_POST['row'],$_POST['due_date'],$category_id]);
+        // タスク追加
+        $task_sql = $pdo->prepare('INSERT INTO Task(title, `row`, state, due_date, create_date, category_id)
+                                    VALUES (?, ?, false, ?, CURRENT_DATE(), ?)');
+        $task_sql->execute([$_POST['title'], $_POST['row'], $_POST['due_date'], $category_id]);
+
+        echo '正常に追加しました';
     }
-    echo '正常に追加しました';
     ?>
     <div class="container">
         <form action="add.php" method="post">
             <input type="text" name="title">
-            <button type="submit">登録</button>
+            <input type="text" name="row">
+            <input type="text" name="due_date">
+            <input type="text" name="category">
+            <button type="submit" name="button">登録</button>
         </form>
         <button type="submit">カレンダー表示</button>
         <button type="button">
@@ -36,4 +43,4 @@
         </button>
     </div>
 </body>
-</html>                                                                                                                                                                 
+</html>
