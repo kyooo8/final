75b541eb1b5818126b52ef7cfd742b1c85664e8f
kyooo8 <kyosuke.7876@gmail.com>
2024-01-13 04:17:23 +0900

    ok

--- src/list.php
+++ src/list.php
@@ -9,22 +9,34 @@
     <?php 
         require 'db.php'; 
         $pdo = new PDO($connect, USER, PASS);
-        $sql = $pdo->query('select * from Task');
-        foreach($sql as $row){
-            $category = $pdo->prepare('select category_name form Category where category_id = ?');
-            $category->execute([$row['category_id']]);
-            $category = $category->fetchColumn();
-            echo '<tr>';
-            echo '<td>',$row['title'],'</td>';
-            echo '<td>',$row['row'],'</td>';
-            echo '<td>',$row['state'],'</td>';
-            echo '<td>',$row['due_date'],'</td>';
-            echo '<td>',$row['create_date'],'</td>';
-            echo '<td>',$category,'</td>';
-            echo '</tr>'; 
-        }
+        $sql = $pdo->query('SELECT * FROM Task');
     ?> 
     <div class="container">
+        <table border="1">
+            <tr>
+                <th>Title</th>
+                <th>Row</th>
+                <th>State</th>
+                <th>Due Date</th>
+                <th>Create Date</th>
+                <th>Category</th>
+            </tr>
+            <?php
+                foreach($sql as $row){
+                    $category = $pdo->prepare('SELECT category_name FROM Category WHERE category_id = ?');
+                    $category->execute([$row['category_id']]);
+                    $category = $category->fetchColumn();
+                    echo '<tr>';
+                    echo '<td>', $row['title'], '</td>';
+                    echo '<td>', $row['row'], '</td>';
+                    echo '<td>', $row['state'], '</td>';
+                    echo '<td>', $row['due_date'], '</td>';
+                    echo '<td>', $row['create_date'], '</td>';
+                    echo '<td>', $category, '</td>';
+                    echo '</tr>'; 
+                }
+            ?>
+        </table>
     </div>
 </body>
 </html>
