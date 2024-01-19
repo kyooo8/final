50d02387c17b027823af387ee6d388bd54832787
kyooo8 <kyosuke.7876@gmail.com>
2024-01-19 13:12:19 +0900

    あ

--- src/list.php
+++ src/list.php
@@ -12,7 +12,7 @@
         $sql = $pdo->query('SELECT * FROM Task');
     ?> 
     <div class="container">
-        <table border="1">
+        <table>
             <tr>
                 <th>Title</th>
                 <th>Row</th>
@@ -21,21 +21,27 @@
                 <th>Create Date</th>
                 <th>Category</th>
             </tr>
-            <?php
-                foreach($sql as $row){
-                    $category = $pdo->prepare('SELECT category_name FROM Category WHERE category_id = ?');
-                    $category->execute([$row['category_id']]);
-                    $category = $category->fetchColumn();
-                    echo '<tr>';
-                    echo '<td>', $row['title'], '</td>';
-                    echo '<td>', $row['row'], '</td>';
-                    echo '<td>', $row['state'], '</td>';
-                    echo '<td>', $row['due_date'], '</td>';
-                    echo '<td>', $row['create_date'], '</td>';
-                    echo '<td>', $category, '</td>';
-                    echo '</tr>'; 
-                }
-            ?>
+            <iframe id="iframe" name="iframe" style="display: none;"></iframe>
+            <form action="" method="post" target="iframe">
+                <?php
+                    foreach($sql as $row){
+                        $category = $pdo->prepare('SELECT category_name FROM Category WHERE category_id = ?');
+                        $category->execute([$row['category_id']]);
+                        $category = $category->fetchColumn();
+                        echo '<tr>';
+                        echo '<td>', $row['title'], '</td>';
+                        echo '<td>', $row['row'], '</td>';
+                        echo '<td>', $row['state'], '</td>';
+                        echo '<td>', $row['due_date'], '</td>';
+                        echo '<td>', $row['create_date'], '</td>';
+                        echo '<td>', $category, '</td>';
+                        echo '<td><input type="submit" name="delete" values="削除"></td>';
+                        echo '<td><input type="submit" name="edit" values="編集"></td>';
+                        echo '</tr>'; 
+                        
+                    }
+                ?>
+            </form>
         </table>
     </div>
 </body>
